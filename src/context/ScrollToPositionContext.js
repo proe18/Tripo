ffa03f25d9cc5499a042767df6cd5106421dea1c
BHieu <nguyenlevietnam2016@gmail.com>
2022-03-13 17:57:32 +0700

    fix scroll to position context

--- src/context/ScrollToPositionContext.js
+++ src/context/ScrollToPositionContext.js
@@ -7,8 +7,9 @@ const ScrollToPositionContext = createContext()
 const ScrollToPositionProvider = ({ children }) => {
     const { pathname } = useLocation()
     const [isActive, setIsActive] = useState(pathname === ROUTES.HOME ? false : undefined)
-    const position = useRef(0)
-    const scrollPosition = position.current
+    const [isEqual, setIsEqual] = useState(false)
+    const scrollToPosition = useRef(0)
+    const positionScroll = scrollToPosition.current
 
     const handlePosition = number => {
         switch (number) {
@@ -22,21 +23,27 @@ const ScrollToPositionProvider = ({ children }) => {
                 setIsActive(undefined)
                 break
         }
-        position.current = number
+        setIsEqual(number === scrollToPosition.current)
+        scrollToPosition.current = number
     }
 
     useEffect(() => {
-        window.scrollTo(0, scrollPosition)
+        //scroll page when 'positionScroll' don't change
+        if (isEqual && window.scrollY !== positionScroll) {
+            window.scrollTo(0, positionScroll)
+            setIsEqual(false)
+        }
+
+        //scroll page when 'positionScroll' change
+        window.scrollTo(0, positionScroll)
         
+        const handleReload = () => {
+            if (window.scrollY !== 0) window.scrollTo(0, 0)
+        }
+
         const timerID = setInterval(() => {
             if (pathname === ROUTES.HOME) setIsActive(window.scrollY >= 4356)
         }, 800)
-
-        const handleReload = () => {
-            if (pathname === ROUTES.HOME) setIsActive(false)
-            window.scrollTo(0, 0)
-            position.current = 0
-        }
 
         window.addEventListener('load', handleReload)
 
@@ -44,7 +51,7 @@ const ScrollToPositionProvider = ({ children }) => {
             window.removeEventListener('load', handleReload)
             clearInterval(timerID)
         }
-    }, [pathname, scrollPosition])
+    }, [pathname, isEqual, positionScroll])
 
     const value = {
         isActive,
